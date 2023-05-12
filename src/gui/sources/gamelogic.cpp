16b9e345b0d9fb1a0df9b07963444077271d019b
mohamedhossam01 <mohamed.hossam.0617@gmail.com>
2023-05-12 09:02:36 +0300

    Separate batch main from GUI code

--- src/gui/sources/gamelogic.cpp
+++ src/gui/sources/gamelogic.cpp
@@ -94,7 +94,7 @@ std::vector<std::pair<int, int>> GameLogic::getAvailableMoves(int playerInTurn)
         }
     }
     log << "Available moves: " << availableMoves.size() << std::endl;
-    for (int i = 0; i < availableMoves.size(); i++)
+    for (int i = 0; i < int(availableMoves.size()); i++)
     {
         log << "\t" << availableMoves[i].first << "," << availableMoves[i].second << std::endl;
     }
@@ -149,7 +149,7 @@ bool GameLogic::isGameOver()
     return getAvailableMoves(WHITE).empty() && getAvailableMoves(BLACK).empty();
 }
 
-// These functions are used for batch testing
+// Any functions below this comment are used for batch testing
 // They are not used in the actual game, but they are useful for debugging
 // They could be removed in the final version when GUI is implemented
 #include <iostream>
@@ -212,85 +212,3 @@ void GameLogic::debug()
     }
     std::cout << horizontal << horizontal << horizontal << horizontal << horizontal << bottomRight << std::endl;
 }
-// int main(int argc, char const *argv[])
-// {
-//     GameLogic game;
-//     game.debug();
-//     int WHITE = game.WHITE;
-//     int BLACK = game.BLACK;
-//     std::vector<std::pair<int, int>> availableMoves;
-//     while (!game.isGameOver())
-//     {
-//         int x, y;
-//         // get available moves
-//         availableMoves = game.getAvailableMoves(BLACK);
-//         for (int i = 0; i < availableMoves.size(); i++)
-//         {
-//             std::cout << i << ". " << availableMoves[i].first << ", " << availableMoves[i].second << std::endl;
-//         }
-//         if (availableMoves.empty())
-//         {
-//             std::cout << "No available moves for black" << std::endl;
-//         }
-//         else
-//         {
-//             while (std::cin >> x >> y)
-//             {
-//                 // check if the move is valid
-//                 bool valid = false;
-//                 for (int i = 0; i < availableMoves.size(); i++)
-//                 {
-//                     if (availableMoves[i].first == x && availableMoves[i].second == y)
-//                     {
-//                         valid = true;
-//                         break;
-//                     }
-//                 }
-//                 if (!valid)
-//                     std::cout << "Invalid move" << std::endl;
-//                 else
-//                     break;
-//             }
-//             game.update(BLACK, x, y);
-//             game.debug();
-//         }
-//         // get available moves
-//         std::vector<std::pair<int, int>> availableMoves = game.getAvailableMoves(WHITE);
-//         for (int i = 0; i < availableMoves.size(); i++)
-//         {
-//             std::cout << i << ". " << availableMoves[i].first << ", " << availableMoves[i].second << std::endl;
-//         }
-//         if (availableMoves.empty())
-//         {
-//             std::cout << "No available moves for white" << std::endl;
-//         }
-//         else
-//         {
-//             while (std::cin >> x >> y)
-//             {
-//                 // check if the move is valid
-//                 bool valid = false;
-//                 for (int i = 0; i < availableMoves.size(); i++)
-//                 {
-//                     if (availableMoves[i].first == x && availableMoves[i].second == y)
-//                     {
-//                         valid = true;
-//                         break;
-//                     }
-//                 }
-//                 if (!valid)
-//                     std::cout << "Invalid move" << std::endl;
-//                 else
-//                     break;
-//             }
-//             game.update(WHITE, x, y);
-//             game.debug();
-//         }
-//     }
-//     std::pair<int, int> score = game.getScore();
-//     std::cout << "Game over" << std::endl;
-//     std::cout << "White: " << score.first << std::endl;
-//     std::cout << "Black: " << score.second << std::endl;
-//     system("pause");
-//     return 0;
-// }
