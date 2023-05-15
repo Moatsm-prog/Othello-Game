8ce4b9948bfc21dc45899768114e3a036c2d231d
ZeusDes <mohamed.ahmed.ma19.ma@gmail.com>
2023-05-15 21:52:17 +0300

    make constructor to include player

--- CornerCapture.h
+++ CornerCapture.h
@@ -1,15 +1,16 @@
 #include "Heuristic.h"
 
 class CornerCapture : public Heuristic {
+public:
+    CornerCapture(int player) : Heuristic(player) {
+    }
+    
     int evaluate(const std::vector<std::vector<int>>& board);
 
     private:
-    const int BLACK = -1;
-    const int WHITE = 1;
     const int BOARD_SIZE = 8;
     const std::vector<std::vector<int>> CORNERS = {{0, 0}, // top left corner
                                                    {0, BOARD_SIZE - 1}, // top right corner
                                                    {BOARD_SIZE - 1, 0}, // bottom left corner 
                                                    {BOARD_SIZE - 1, BOARD_SIZE - 1}}; // bottom right corner
-    // TODO: Assign weights to the four corners
 };
--- Heuristic.h
+++ Heuristic.h
@@ -1,6 +1,16 @@
 #include <vector>
 
 class Heuristic {
-    public:
+public:
+    explicit Heuristic(int player) : player(player) {}
+
     virtual int evaluate(const std::vector<std::vector<int>>& board) const = 0;
+
+protected:
+    int getPlayer() const {
+        return player;
+    }
+
+private:
+    int player;
 };
