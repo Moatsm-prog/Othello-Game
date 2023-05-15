ab5366b29ff464cff2d6ca0d45f08b922713e475
mohamedhossam01 <mohamed.hossam.0617@gmail.com>
2023-05-15 13:07:19 +0300

    Several UI Enhancements

--- src/gui/headers/macros.h
+++ src/gui/headers/macros.h
@@ -1,7 +1,7 @@
 #ifndef MACROS_H
 #define MACROS_H
-#define BLACK_PIECE "QWidget{background-color: rgb(0, 0, 0);min-width:50;max-width:50;min-height:50;max-height:50;border-radius:25;border: 2px solid qradialgradient(spread:pad, cx:0.5, cy:0.5, radius:1.862, fx:0.5, fy:0.5, stop:0 rgba(9, 6, 13, 255), stop:1 rgba(255, 255, 255, 255));border-top: black;border-bottom: black;}"
-#define WHITE_PIECE "QWidget{ background-color: rgb(223, 223, 223); min-width:50; max-width:50; min-height:50; max-height:50; border-radius:25;border: 2px solid qradialgradient(spread:pad, cx:0.5, cy:0.5, radius:2.5, fx:0.7, fy:0.7, stop:0 rgba(255, 255, 255, 255), stop:1 rgba(9, 6, 13, 255));border-top: black;border-bottom: black;}"
+#define BLACK_PIECE "QWidget {background-color: qradialgradient(spread: pad, cx: 0.5, cy: 0.5, radius: 1,fx: 0.5, fy: 0.5,stop: 0 rgba(33, 33, 33, 255),stop: 0.87 rgba(33, 33, 33, 255),stop: 1 rgba(13, 13, 13, 255));min-width: 50px;max-width: 50px;min-height: 50px;max-height: 50px;border-radius: 25px;border-width: 2px;border-style: solid;border-color: #111111;}"
+#define WHITE_PIECE "QWidget {background-color: qradialgradient(spread: pad, cx: 0.5, cy: 0.5, radius: 1,fx: 0.5, fy: 0.5,stop: 0 rgba(235, 235, 235, 255),stop: 0.87 rgba(235, 235, 235, 255),stop: 1 rgba(255, 255, 255, 255));min-width: 50px;max-width: 50px;min-height: 50px;max-height: 50px;border-radius: 25px;border-width: 2px;border-style: solid;border-color: #111111;}"
 #define INIDICATOR_PIECE "QWidget{ background-color: rgb(97, 199, 187); min-width:20; max-width:20; min-height:20; max-height:20; border-radius:10;}"
 #define BLACK 1
 #define WHITE 0
